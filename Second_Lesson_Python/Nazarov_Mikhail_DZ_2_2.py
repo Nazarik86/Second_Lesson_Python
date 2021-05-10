afbc1dd3ff91c1dbfd600dfdf8f157a4d19e330b
Mikhail <mikhail.nazarov@internet.ru>
2021-05-10 12:39:16 +0300

    Done

--- Second_Lesson_Python/Nazarov_Mikhail_DZ_2_2.py
+++ Second_Lesson_Python/Nazarov_Mikhail_DZ_2_2.py
@@ -6,3 +6,27 @@
 # Новый список не создавать! Сформировать из обработанного списка строку:
 # в "05" часов "17" минут температура воздуха была "+05" градусов
 
+default_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+
+i = 0
+while i < len(default_list):
+    if default_list[i].isdigit() or default_list[i][1:].isdigit():
+        if default_list[i][0] == '+' or default_list[i][0] == '-':
+            default_list_sum = default_list[i][0]
+            default_list[i] = default_list_sum + default_list[i][1:].zfill(2)
+        default_list[i] = default_list[i][1:].zfill(2)
+        default_list.insert(i, '"')
+        default_list.insert(i + 2, '"')
+        i = i + 2
+    i = i + 1
+
+i = 0
+while i < len(default_list):
+    if default_list[i].isdigit() or default_list[i][1:].isdigit():
+        default_list[i] = default_list[i - 1] + default_list[i] + default_list[i + 1]
+        default_list.pop(i + 1)
+        default_list.pop(i - 1)
+    i += 1
+
+output_default_list = ' '.join(default_list)
+print(output_default_list)
