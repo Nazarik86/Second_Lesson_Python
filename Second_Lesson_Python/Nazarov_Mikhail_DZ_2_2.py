ba54a1da6c698830388932fb7eaad26293d21665
Mikhail <mikhail.nazarov@internet.ru>
2021-05-11 11:54:22 +0300

    С ковычками так и не разобрался, в выведенном списке, но после просмотра варианта решения, понял что изначально не так начал делать. Смысла нет копипастить и поэтому сдам как есть в сыром варианте. С выводами промежуточных значений.

--- Second_Lesson_Python/Nazarov_Mikhail_DZ_2_2.py
+++ Second_Lesson_Python/Nazarov_Mikhail_DZ_2_2.py
@@ -6,27 +6,40 @@
 # Новый список не создавать! Сформировать из обработанного списка строку:
 # в "05" часов "17" минут температура воздуха была "+05" градусов
 
+# С ковычками так и не разобрался, в выведенном списке, но после просмотра варианта решения,
+# понял что изначально не так начал делать. Смысла нет копипастить и поэтому сдам как есть в сыром варианте.
+# С выводами промежуточных значений.
+
 default_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+new_list = []
 
 i = 0
-while i < len(default_list):
+
+for i in range(len(default_list)):
     if default_list[i].isdigit() or default_list[i][1:].isdigit():
+        number = int(default_list[i])
         if default_list[i][0] == '+' or default_list[i][0] == '-':
             default_list_sum = default_list[i][0]
             default_list[i] = default_list_sum + default_list[i][1:].zfill(2)
-        default_list[i] = default_list[i][1:].zfill(2)
-        default_list.insert(i, '"')
-        default_list.insert(i + 2, '"')
-        i = i + 2
-    i = i + 1
+            print('Вывод с +/-: ', default_list[i])
+            new_list.append(default_list[i])
+        elif number > 0:
+            print('default_list[i]: ', default_list[i])
+            print('number: ', number)
+            print('zfill: ', default_list[i].zfill(2))
+            print((type(number)))
+            new_list.append(default_list[i].zfill(2))
+    else:
+        new_list.append(default_list[i])
 
-i = 0
-while i < len(default_list):
-    if default_list[i].isdigit() or default_list[i][1:].isdigit():
-        default_list[i] = default_list[i - 1] + default_list[i] + default_list[i + 1]
-        default_list.pop(i + 1)
-        default_list.pop(i - 1)
-    i += 1
+print('new_list: ', new_list)
+new_list_sum = []
 
-output_default_list = ' '.join(default_list)
-print(output_default_list)
+for i in range(len(new_list)):
+    if new_list[i].isdigit() or new_list[i][1:].isdigit():
+        new_sum = new_list[i]
+        new_list_sum = ' '.join(f'"{new_sum}"' for new_sum in new_list[i])
+        # new_list_sum.insert(i, '"')
+        # new_list_sum.insert(i + 2, '"')
+        print('new_sum: ', new_sum)
+        print('new_list_sum', new_list_sum)
