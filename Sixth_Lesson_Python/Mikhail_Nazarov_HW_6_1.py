302a3bc03a5e635cfd3d62cfd5d16937c6cb3152
Mikhail <mikhail.nazarov@internet.ru>
2021-05-26 10:16:44 +0300

    Create directory for Sixth Lesson

---
+++ Sixth_Lesson_Python/Mikhail_Nazarov_HW_6_1.py
@@ -0,0 +1,27 @@
+# 1. Не используя библиотеки для парсинга,
+# распарсить (получить определённые данные) файл логов web-сервера nginx_logs.txt
+#
+# (https://github.com/elastic/examples/raw/master/Common%20Data%20Formats/nginx_logs/nginx_logs)
+# — получить список кортежей вида: (<remote_addr>, <request_type>, <requested_resource>). Например:
+#
+# [
+# ...
+# ('141.138.90.60', 'GET', '/downloads/product_2'),
+# ('141.138.90.60', 'GET', '/downloads/product_2'),
+# ('173.255.199.22', 'GET', '/downloads/product_2'),
+# ...
+# ]
+#
+# Примечание: код должен работать даже с файлами, размер которых превышает объем ОЗУ компьютера.
+
+remote_addr = []
+request_type = []
+requested_resource = []
+
+with open('nginx_logs.txt', encoding='utf-8') as logs_file:
+    for line in logs_file:
+        remote_addr.append(line.split()[0])
+        request_type.append(line.split()[5].replace('"', ''))
+        requested_resource.append(line.split()[6])
+summary = list(zip(remote_addr, request_type, requested_resource))
+print(summary)
---
+++ Sixth_Lesson_Python/Mikhail_Nazarov_HW_6_6.py
@@ -0,0 +1,39 @@
+# 6. Реализовать простую систему хранения данных о суммах продаж булочной.
+# Должно быть два скрипта с интерфейсом командной строки: для записи данных и для вывода на экран записанных данных.
+# При записи передавать из командной строки значение суммы продаж.
+# Для чтения данных реализовать в командной строке следующую логику:
+#
+#     просто запуск скрипта — выводить все записи;
+#     запуск скрипта с одним параметром-числом — выводить все записи с номера, равного этому числу, до конца;
+#     запуск скрипта с двумя числами — выводить записи,
+#     начиная с номера, равного первому числу, по номер, равный второму числу, включительно.
+
+import math
+import sys
+
+LINE_LENGTH = 7
+show_from_line = 1
+show_to_line = math.inf
+
+
+def show_sales(show_from_line=1, show_to_line=math.inf):
+    if show_from_line < 2:
+        show_from_position = 0
+    else:
+        show_from_position = (show_from_line - 1) * LINE_LENGTH
+    show_to_position = show_to_line * LINE_LENGTH
+
+    with open('bakery.csv', encoding='utf-8') as f:
+        f.seek(show_from_position)
+        line = f.readline()
+        while line:
+            print(line.strip())
+            if show_to_position < f.tell():
+                break
+            line = f.readline()
+
+
+if __name__ == '__main__':
+    arguments = (int(arg) for arg in sys.argv[1:])
+
+    show_sales(*arguments)
